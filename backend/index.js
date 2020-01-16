3930aba5de211e60bcd31f2ab00457369377a16f
Fernando-Braulio <fernandobraulio@outlook.com.br>
2020-01-16 14:04:24 -0300

    backend start

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+//Métodos HTTP: get, post, put, delete
+
+//Tipos de parametros:
+//Query Params: request.Query (são usados para filtros, ordenação, paginação....)
+//Route Params: request.Params (são usados para identificar um recurso na alteração ou delete)
+//Body: request.Body (são dados para criação ou alteração de registro)
+
+
+app.post('/users', (request, response) => {
+    console.log(request.body);
+    return response.json({ message: 'Hello OmniStack' });
+});
+
+app.listen(3333);
