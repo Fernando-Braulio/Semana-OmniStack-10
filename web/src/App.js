cc2da8ada520f3cdcc58a298bada7ede0f3cb804
Fernando-Braulio <fernandobraulio@outlook.com.br>
2020-01-16 14:46:55 -0300

    deleting files you don't need

--- web/src/App.js
+++ web/src/App.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+
+//Componente: bloco isolado de html, css e js no qual nao intefere no restante da aplicação
+//Propriedade: Imformações que um componente PAI passa pra o componente FILHO
+//Estado: 
 
 function App() {
+
+  const [counter, setCounter] = useState(0);
+
+  function incrementCounter() {
+    counter++;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Contador: {counter}</h1>
+      <button onClick={incrementCounter}>Incrementar</button>
+    </>
   );
 }
 
