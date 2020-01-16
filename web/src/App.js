21351f91b485c928d9014bbf28c348361559bb41
Fernando-Braulio <fernandobraulio@outlook.com.br>
2020-01-16 15:37:15 -0300

    aside form

--- web/src/App.js
+++ web/src/App.js
@@ -1,22 +1,48 @@
-import React, {useState} from 'react';
+import React from 'react';
+import './global.css';
+import './App.css';
+import './Sidebar.css';
 
 //Componente: bloco isolado de html, css e js no qual nao intefere no restante da aplicação
 //Propriedade: Imformações que um componente PAI passa pra o componente FILHO
-//Estado: 
+//Estado: Informações mantidas pelo componente (Lembrar: imutabilidade)
 
 function App() {
-
-  const [counter, setCounter] = useState(0);
-
-  function incrementCounter() {
-    counter++;
-  }
-
   return (
-    <>
-      <h1>Contador: {counter}</h1>
-      <button onClick={incrementCounter}>Incrementar</button>
-    </>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do GitHub</label>
+            <input name="github_username" id="github_username" required/>  
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologia</label>
+            <input name="techs" id="techs" required/>
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required/>
+            </div>
+            
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required/>
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      
+      <main>
+
+      </main>
+    </div>
   );
 }
 
