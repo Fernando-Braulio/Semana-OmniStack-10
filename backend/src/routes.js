3c7238ff22bf3303822d01a2c9827689b6eae2e0
Fernando-Braulio <fernandobraulio@outlook.com.br>
2020-01-17 11:07:12 -0300

    get users

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,33 +1,9 @@
 const {Router} = require('express');
-const axios = require('axios');
-const Dev = require('./models/Dev');
+const DevControllers = require('./controllers/DevController');
 
 const routes = Router();
 
-routes.post('/devs', async (request, response) => {
-    const { github_username, techs, latitude, longitude } = request.body;
-
-    const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
-
-    const { name = login, avatar_url, bio } = apiResponse.data;
-
-    const techsArray = techs.split(',').map(tech => tech.trim());
-
-    const location = {
-        type: 'Point',
-        coordinates: [longitude, latitude],
-    }
-
-    const dev = await Dev.create({
-        github_username,
-        name,
-        avatar_url,
-        bio,
-        techs: techsArray,
-        location
-    });
-    
-    return response.json(dev);
-});
+routes.get('/devs', DevControllers.index);
+routes.post('/devs', DevControllers.store);
 
 module.exports = routes;
