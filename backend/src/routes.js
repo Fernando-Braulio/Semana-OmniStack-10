0aef62e80f46d39c2118234e97619b2a88d5da5d
Fernando-Braulio <fernandobraulio@outlook.com.br>
2020-01-17 11:22:08 -0300

    testing search

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,12 @@
 const {Router} = require('express');
 const DevControllers = require('./controllers/DevController');
+const SearchControllers = require('./controllers/SearchController');
 
 const routes = Router();
 
 routes.get('/devs', DevControllers.index);
 routes.post('/devs', DevControllers.store);
+
+routes.get('/search', SearchControllers.index);
 
 module.exports = routes;
